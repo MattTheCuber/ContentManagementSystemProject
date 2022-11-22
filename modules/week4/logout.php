393acd6bf54c4c422305d3c51fa024e378ddb01c
MattTheCuber <mattvine03@gmail.com>
2022-11-22 17:36:17 -0500

    fix: maybe

--- modules/week4/logout.php
+++ modules/week4/logout.php
@@ -2,7 +2,6 @@
    session_start();
    unset($_SESSION["username"]);
    unset($_SESSION["loginlevel"]);
-   session_destroy();
    
    echo 'You have logged out';
    header('Location: https://matthewvine.site');
